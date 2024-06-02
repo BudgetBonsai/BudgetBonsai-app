80d493d0dd69e922307f086d096690e7907ef8ad
Efilon <naufalyasir5467@gmail.com>
2024-06-02 23:24:55 +0700

    test

--- app/src/main/java/com/example/budgetbonsai/SplashActivity.kt
+++ app/src/main/java/com/example/budgetbonsai/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        //test
         viewModel.startSplashTimer()
 
         viewModel.isTimerFinished.observe(this) { isFinished ->
